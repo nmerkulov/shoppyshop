eb73588b347acee8ed58ba6a9f0db09cb0eb7170
Vasilii Emelin <yemelin@gmail.com>
2020-07-05 20:26:38 +0300

    disable link preview
    Signed-off-by: Vasilii Emelin <yemelin@gmail.com>

--- .github/scripts/notify_tg.sh
+++ .github/scripts/notify_tg.sh
@@ -6,4 +6,4 @@ CHAT_ID=-1001174776301
 
 BRANCH_NAME=${GITHUB_REF##*/}
 curl -X POST https://api.telegram.org/bot${BOT_ID}/sendMessage -d chat_id=${CHAT_ID} \
--d "text=\`${GITHUB_ACTOR}\` *successfully* __pushed__ branch [${BRANCH_NAME//-/\\-}](https://github.com/${GITHUB_REPOSITORY}/tree/${BRANCH_NAME//-/\\-})" -d parse_mode=MarkdownV2
+-d "text=\`${GITHUB_ACTOR}\` *successfully* __pushed__ branch [${BRANCH_NAME//-/\\-}](https://github.com/${GITHUB_REPOSITORY}/tree/${BRANCH_NAME//-/\\-})" -d parse_mode=MarkdownV2 -d disable_web_page_preview=true
