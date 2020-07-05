29da97f3e6abe1631da7fc1a4252210d53dc92ca
Vasilii Emelin <yemelin@gmail.com>
2020-07-05 20:18:17 +0300

    markdown
    Signed-off-by: Vasilii Emelin <yemelin@gmail.com>

--- .github/scripts/notify_tg.sh
+++ .github/scripts/notify_tg.sh
@@ -4,5 +4,6 @@
 BOT_ID=576734993:AAFhJQALvnwzXR8ZWlLGkRaZePlUcGGwMmA
 CHAT_ID=-1001174776301
 
+BRANCH_NAME=${GITHUB_REF##*/}
 curl -X POST https://api.telegram.org/bot${BOT_ID}/sendMessage -d chat_id=${CHAT_ID} \
--d "text=<${GITHUB_ACTOR}> successfully pushed branch <${GITHUB_REF##*/}>"
+-d "text=\`${GITHUB_ACTOR}\` *successfully* __pushed__ branch [${BRANCH_NAME//-/\\-}](https://github.com/${GITHUB_REPOSITORY}/tree/${BRANCH_NAME//-/\\-})" -d parse_mode=MarkdownV2
